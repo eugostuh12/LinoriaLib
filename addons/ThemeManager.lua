7cc11689fbdbca76918c02d20d56f7e376bc2a71
eugostuh12 <eleedemaiscara@gmail.com>
2025-11-18 17:46:09 -0300

    Add comment to ThemeManager.lua

--- addons/ThemeManager.lua
+++ addons/ThemeManager.lua
@@ -1,3 +1,5 @@
+--nada de mais mizera
+
 local cloneref = (cloneref or clonereference or function(instance: any) return instance end)
 local httpService = cloneref(game:GetService('HttpService'))
 local httprequest = (syn and syn.request) or request or http_request or (http and http.request)
